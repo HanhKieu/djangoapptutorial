34554e433ac1cdf5ad9bddc396d611b0dd74f28f
HanhKieu <hckieu@ucdavis.edu>
2014-09-20 03:56:07 -0700

    made admin page all cool n shit

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from polls.models import  Question , Choice
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3 #provide enough fields for 3 questions
+    #this class lets us to input choice only after we make question.
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    fields = ['pub_date', 'question_text']
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question,QuestionAdmin)
 
 # Register your models here.
